a5656b2638a4cc4b1bb0fc0a657978dccd66bcf3
Mazuh <marcell-mz@hotmail.com>
2017-03-17 18:06:43 -0300

    Init vs project

---
+++ AutoScanner/AutoScanner/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there");
+            Console.ReadLine();
+        }
+    }
+}
